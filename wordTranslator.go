e430c7fb3ae79de81a9203c8a88c40db76ae3297
Xabi <xmartinez1702@gmail.com>
2018-10-06 13:19:02 +0200

    Create wordTranslator

---
+++ wordTranslator.go
@@ -0,0 +1,58 @@
+package translator
+
+import (
+	"math"
+	"strings"
+	"unicode"
+)
+
+type WordTranslator interface {
+	translateWord(string) string
+}
+
+const (
+	sufix      string = "ay"
+	vowelSufix string = "w" + sufix
+)
+
+type pigLatingWordTranslator struct {
+}
+
+func (t *pigLatingWordTranslator) TranslateWord(word string) string {
+	firstLetter := []rune(word)[0]
+
+	if t.isVowel(firstLetter) {
+		return word + vowelSufix
+	}
+
+	firstVowel := t.getFirstVowelPosition(word)
+
+	if firstLetter == unicode.ToUpper(firstLetter) {
+		return strings.Title(word[firstVowel:]) + strings.ToLower(word[:firstVowel]) + sufix
+	}
+	return word[firstVowel:] + word[:firstVowel] + sufix
+}
+
+func (t *pigLatingWordTranslator) getFirstVowelPosition(word string) int {
+	lowerStr := strings.ToLower(word)
+	isOdd := len(lowerStr)%2 != 0
+	middle := int(math.Ceil(float64(len(lowerStr)) / 2))
+	startsQ := lowerStr[0] == 'q'
+
+	for i, c := range lowerStr {
+		if (t.isVowel(c) || ('y' == c && isOdd && i+1 == middle)) && !('u' == c && i == 1 && startsQ) {
+			return i
+		}
+	}
+
+	return 0
+}
+
+func (t *pigLatingWordTranslator) isVowel(character rune) bool {
+	switch character {
+	case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+		return true
+	}
+
+	return false
+}
---
+++ wordTranslator_test.go
@@ -0,0 +1,42 @@
+package translator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var translateWordToPigLatingTests = []struct {
+	testTitle    string
+	text         string
+	expectedText string
+}{
+	{"starts with volwel", "is", "isway"},
+	{"starts with consonant", "rose", "oseray"},
+	{"starts with consonant 'y' in middle", "style", "ylestay"},
+	{"starts with consonant 'u' as consonant", "question", "estionquay"},
+	{"starts with consonant and capitalized", "Question", "Estionquay"},
+	{"starts with consonant and no vowel letter", "chty", "chtyay"},
+}
+
+func TestPigLatinTranslatorGivenTextTranslatesIt(t *testing.T) {
+	translator := pigLatingWordTranslator{}
+	for _, test := range translateWordToPigLatingTests {
+		t.Run(test.testTitle, func(t *testing.T) {
+			translation := translator.TranslateWord(test.text)
+			assert.Equal(t, test.expectedText, translation)
+		})
+	}
+}
+
+func TestGivenWordStartedWithVowel(t *testing.T) {
+	translator := pigLatingWordTranslator{}
+
+	assert.Equal(t, "isway", translator.TranslateWord("is"))
+}
+
+func TestGivenWordStartedWithConsonant(t *testing.T) {
+	translator := pigLatingWordTranslator{}
+
+	assert.Equal(t, "isway", translator.TranslateWord("is"))
+}
